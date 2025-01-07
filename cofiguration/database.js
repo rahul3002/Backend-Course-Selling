8e77242d3756cd4c95231216c0c55783577d6fdf
rahul3002 <rahulbagal7171@gmail.com>
2025-01-07 19:34:17 +0530

    mongodb collection created

--- cofiguration/database.js
+++ cofiguration/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const connectDb = async () => {
-    await mongoose.connect("mongodb+srv://rahulbagal7171:8UQgwSQ5g6pYT84A@cluster0.ot6nw.mongodb.net/Testing")
+    await mongoose.connect("mongodb+srv://rahulbagal7171:8UQgwSQ5g6pYT84A@cluster0.ot6nw.mongodb.net/CourseWebsite")
 
 }
 module.exports = connectDb
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 const port =  5000;
+const {UserModel, AdminModel, CourseModel, PurchaseModel} = require("./Schema/db");
 const connectDb = require("./cofiguration/database");
 const adminRouter = require("./routes/admin");
 const userRouter = require("./routes/user");
 const courseRouter = require("./routes/courses");
+const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use("/admin", adminRouter);
 app.use("/user", userRouter);
